72404e1f525a39b0a1d0025ef989c05bd983fe16
Thanhmai1 <test@w3schools.com>
2024-10-28 11:46:29 +0700

    Add project files.

---
+++ Controllers/ProductSingleController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Book_Shop.Controllers
+{
+	[Route("product-single")]
+	public class Product_Single : Controller
+	{
+		[HttpGet("")]
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ Controllers/ShopController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Book_Shop.Controllers
+{
+    [Route("shop")]
+    public class ShopController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
